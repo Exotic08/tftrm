ca86afdfcca64d8f158752d610ad1cc817f55f73
Exotic08 <lanhtu2010@gmail.com>
2025-12-12 06:38:09 +0700

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// js/main.js
-import { GameManager } from './gameManager.js';
-
-// Khởi chạy game khi DOM đã sẵn sàng
-window.addEventListener('DOMContentLoaded', () => {
-    new GameManager();
-});
